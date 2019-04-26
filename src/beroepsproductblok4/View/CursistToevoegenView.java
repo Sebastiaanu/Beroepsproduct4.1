8dc352fc46fea85a068d9941e5c0f62a4b51e73d
jelmu <jelmuis@gmail.com>
2019-04-26 12:12:38 +0200

    Aan de slag met views - toevoegen vrijwilliger, cursist & buurthuis. Done - verwijderen vrijwilliger, cursist & buurthuis. Done
    Koppeling met database is nog niet gemaakt.

--- src/beroepsproductblok4/View/CursistToevoegenView.java
+++ src/beroepsproductblok4/View/CursistToevoegenView.java
@@ -22,12 +22,14 @@ public class CursistToevoegenView extends GridPane {
     public Text lblSureName;
     public Text lblEmail;
     public Text lblPhonenr;
+    public Text lblCountryOfOrigin;
             
     public TextField txtFirstName;
     public TextField txtTussenvoegsel;
     public TextField txtSureName;
     public TextField txtEmail;
     public TextField txtPhonenr;
+    public TextField txtCountryOfOrigin;
     
     public Button addPerson;
     
@@ -37,12 +39,15 @@ public class CursistToevoegenView extends GridPane {
     lblSureName = new Text("Achternaam cursist: ");
     lblEmail = new Text("Email cursist: ");
     lblPhonenr = new Text("Telefoonnummer cursist: ");
+    lblCountryOfOrigin = new Text("Land van herkomst");
     
     txtFirstName = new TextField();
     txtTussenvoegsel = new TextField();
     txtSureName = new TextField();
     txtEmail = new TextField();
     txtPhonenr = new TextField();
+    txtCountryOfOrigin = new TextField();
+    
     
     addPerson = new Button("Cursist toevoegen");
     
@@ -54,14 +59,16 @@ public class CursistToevoegenView extends GridPane {
     add(lblSureName,0,2);
     add(lblEmail,0,3);
     add(lblPhonenr,0,4);
+    add(lblCountryOfOrigin,0,5);
     
     add(txtFirstName,1,0);
     add(txtTussenvoegsel,1,1);
     add(txtSureName,1,2);
     add(txtEmail,1,3);
     add(txtPhonenr,1,4);
+    add(txtCountryOfOrigin,1,5);
     
-    add(addPerson,1,5);
+    add(addPerson,1,6);
     
     p.getChildren().addAll(this);
     }
